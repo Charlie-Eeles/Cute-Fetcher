fb1886b3649aaaa2136b00767ecba026119325cc
charlie <charlie.eeles@hotmail.com>
2018-10-24 16:39:41 +0100

    Updated extension filter
    If a gifv filetype is returned by the fetch, a new fetch will be called instead.
    typeof "null" is now filtered out.

--- CuteJS.js
+++ CuteJS.js
@@ -2,11 +2,11 @@ let dog = document.querySelector("#dog-picture");
 let cat = document.querySelector("#cat-picture");
 let other = document.querySelector("#other-picture");
 
-let y = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens"];
+let y = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens","CatsStandingUp", "tuckedinkitties", "catbellies"];
 
 
 function randomSub(){
-    return Math.floor(Math.random() * Math.floor(4));
+    return Math.floor(Math.random() * Math.floor(7));
 };
 
 function randomPic(){
@@ -20,11 +20,11 @@ other.style.display = "none";
 function fetcher(sub,x) {fetch(`https://www.reddit.com/r/${sub}.json?limit=${x}`)
     .then (response => response.json())
     .then (data => data.data.children.map(data => data.data.url))
-    .then (map => {while (typeof map[x] ==="undefined" || map[x].indexOf("comment")>-1 || map[x].indexOf("gallery")>-1 || map[x].indexOf("/a/")>-1 || map[x].indexOf("v.redd.it")>-1){x = randomPic()}return map[x]})
-    .then (img => {if (img.split('.').pop() !== "png" && img.split('.').pop() !== "jpg" && img.split('.').pop() !== "gif")
-                    {if(img.split('.').pop() === "gifv")
-                    {img.split("gifv").pop()}
-                    img +=".gif"}; return img;})
+    .then (map => {while (typeof map[x] ==="undefined"|| typeof map[x] ==="null" || map[x].indexOf("comment")>-1 || map[x].indexOf("gallery")>-1 || map[x].indexOf("/a/")>-1 || map[x].indexOf("v.redd.it")>-1 || map[x].indexOf("gifv")>-1){x = randomPic()}return map[x]})
+    .then (img => {if (img.split('.').pop() !== "jpg" && img.split('.').pop() !== "gif" && img.split('.').pop() !== "gifv" && img.split('.').pop() !== "png" )
+                    {img +=".gif"};
+                    return img;})
+                  
     .then (img => cat.src=img)
 }; 
 
