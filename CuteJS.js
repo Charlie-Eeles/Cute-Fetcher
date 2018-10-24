40bb9b15fdb7b65f7cff0865b3acd8fab7221314
charlie <charlie.eeles@hotmail.com>
2018-10-24 11:35:53 +0100

    Added click event/randomiser
    At this stage if you click the button it will return a random image from a selection of subs (for the cat button), need to implement extension check.

--- CuteJS.js
+++ CuteJS.js
@@ -1,17 +1,27 @@
 let dog = document.querySelector("#dog-picture");
 let cat = document.querySelector("#cat-picture");
 let other = document.querySelector("#other-picture");
+let rando = 
 
 dog.style.display = "none";
 other.style.display = "none";
 
     
-    function butt(x) {fetch(`https://www.reddit.com/r/catsinbusinessattire.json?limit=20`)
+function butt(sub,x) {fetch(`https://www.reddit.com/r/${sub}.json?limit=${x}`)
     .then (response => response.json())
     .then (data => data.data.children.map(data => data.data.url))
     .then (map => map[x])
     .then (img => cat.src=img)
-        }; 
+}; 
 
-    butt(8);
-// cat.src=butt(1);
+function dude(){
+    let x = Math.floor(Math.random() * Math.floor(4));
+    let y = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens"]
+    let z = Math.floor(Math.random() * Math.floor(100));
+    return butt(y[x],z);
+}
+    
+document.getElementById("cat-button").addEventListener("click", function(){
+    
+    dude(); 
+});
