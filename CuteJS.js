6728ec69b62fc336344e27fabb013b129970432b
charlie <charlie.eeles@hotmail.com>
2018-10-24 20:48:17 +0100

    changed filters/border-radius/animal img tags html
    added some more filters for weird links.
    added an if statement to prevent non-imgur and non-native reddit images from displaying.
    removed unecessary img-sources in html.
    reduced image border radius in css.

--- CuteJS.js
+++ CuteJS.js
@@ -1,30 +1,43 @@
-let dog = document.querySelector("#dog-picture");
 let cat = document.querySelector("#cat-picture");
-let other = document.querySelector("#other-picture");
-
-let y = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens","CatsStandingUp", "tuckedinkitties", "catbellies"];
 
 
-function randomSub(){
-    return Math.floor(Math.random() * Math.floor(7));
+let catSubs = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens","CatsStandingUp", "tuckedinkitties", "catbellies", "blep"];
+let dogSubs = ["dogpictures", "goldenretrievers","puppies","rarepuppers","blop","BabyCorgis","PuppySmiles","TongueOutHounds"];
+let otherSubs = ["redpandas","BabySkunks", "hamsters","Sneks","hardcoreaww","babygoats","Superbowl"];
+
+function randomSub(x){
+    return Math.floor(Math.random() * Math.floor(x));
 };
 
 function randomPic(){
     return Math.floor(Math.random() * Math.floor(30));
 };
 
-dog.style.display = "none";
-other.style.display = "none";
 
     
 function fetcher(sub,x) {fetch(`https://www.reddit.com/r/${sub}.json?limit=${x}`)
     .then (response => response.json())
     .then (data => data.data.children.map(data => data.data.url))
-    .then (map => {while (typeof map[x] ==="undefined"|| typeof map[x] ==="null" || map[x].indexOf("comment")>-1 || map[x].indexOf("gallery")>-1 || map[x].indexOf("/a/")>-1 || map[x].indexOf("v.redd.it")>-1 || map[x].indexOf("gifv")>-1){x = randomPic()}return map[x]})
+    .then (map => {while (  typeof map[x] ==="undefined"||
+                            typeof map[x] ==="null" ||
+                            map[x].indexOf("comment")>-1 ||
+                            map[x].indexOf("gallery")>-1 || 
+                            map[x].indexOf("/a/")>-1 ||
+                            map[x].indexOf("v.redd.it")>-1 || 
+                            map[x].indexOf("gifv")>-1 ||
+                            map[x].indexOf(".html")>-1 ||
+                            map[x].indexOf("youtu")>-1 ||
+                            map[x].indexOf("gfy")>-1 ||
+                            map[x].indexOf("embed.ly")>-1
+                           )
+                            {x = randomPic()};
+                            if(map[x].indexOf("imgur")===-1 && map[x].indexOf("i.redd.it")===-1){
+                                return "https://i.imgur.com/87svlNR.jpg";
+                            }else{
+                        return map[x]};})
     .then (img => {if (img.split('.').pop() !== "jpg" && img.split('.').pop() !== "gif" && img.split('.').pop() !== "gifv" && img.split('.').pop() !== "png" )
                     {img +=".gif"};
                     return img;})
-                  
     .then (img => cat.src=img)
 }; 
 
@@ -32,6 +45,13 @@ function fetcher(sub,x) {fetch(`https://www.reddit.com/r/${sub}.json?limit=${x}`
 
 
 document.getElementById("cat-button").addEventListener("click", function(){
-    fetcher (y[randomSub()], randomPic());
+    fetcher (catSubs[randomSub(8)], randomPic());
 });
 
+document.getElementById("dog-button").addEventListener("click", function(){
+    fetcher (dogSubs[randomSub(8)], randomPic());
+});
+
+document.getElementById("other-button").addEventListener("click", function(){
+    fetcher (otherSubs[randomSub(7)], randomPic());
+});
