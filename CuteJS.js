8661a7e449bf400b362965c431eb78ae0af1461f
charlie <charlie.eeles@hotmail.com>
2018-10-24 15:43:30 +0100

    added filetype checks
    Made it so if the fetch returns undefined, a gallery, album or reddit video a new link will be fetched instead.
    adds .gif to files without extensions.
    
    problem in this commit -
    gif is added to gifv when I would like it to replace gifv instead.

--- CuteJS.js
+++ CuteJS.js
@@ -1,27 +1,37 @@
 let dog = document.querySelector("#dog-picture");
 let cat = document.querySelector("#cat-picture");
 let other = document.querySelector("#other-picture");
-let rando = 
+
+let y = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens"];
+
+
+function randomSub(){
+    return Math.floor(Math.random() * Math.floor(4));
+};
+
+function randomPic(){
+    return Math.floor(Math.random() * Math.floor(30));
+};
 
 dog.style.display = "none";
 other.style.display = "none";
 
     
-function butt(sub,x) {fetch(`https://www.reddit.com/r/${sub}.json?limit=${x}`)
+function fetcher(sub,x) {fetch(`https://www.reddit.com/r/${sub}.json?limit=${x}`)
     .then (response => response.json())
     .then (data => data.data.children.map(data => data.data.url))
-    .then (map => map[x])
+    .then (map => {while (typeof map[x] ==="undefined" || map[x].indexOf("comment")>-1 || map[x].indexOf("gallery")>-1 || map[x].indexOf("/a/")>-1 || map[x].indexOf("v.redd.it")>-1){x = randomPic()}return map[x]})
+    .then (img => {if (img.split('.').pop() !== "png" && img.split('.').pop() !== "jpg" && img.split('.').pop() !== "gif")
+                    {if(img.split('.').pop() === "gifv")
+                    {img.split("gifv").pop()}
+                    img +=".gif"}; return img;})
     .then (img => cat.src=img)
 }; 
 
-function dude(){
-    let x = Math.floor(Math.random() * Math.floor(4));
-    let y = ["CatsInBusinessAttire", "catpictures", "jellybeantoes", "kittens"]
-    let z = Math.floor(Math.random() * Math.floor(100));
-    return butt(y[x],z);
-}
-    
+
+
+
 document.getElementById("cat-button").addEventListener("click", function(){
-    
-    dude(); 
+    fetcher (y[randomSub()], randomPic());
 });
+
